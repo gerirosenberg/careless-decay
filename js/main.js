e35c6e7fdfcfa022b4b8e8ea9855d344babffd6d
mattrode <44823090+mattrode@users.noreply.github.com>
2019-04-17 23:43:22 -0400

    colors

--- js/main.js
+++ js/main.js
@@ -16,7 +16,7 @@ $header_top.find('a').on('click', function() {
 
 // fullpage customization
 $('#fullpage').fullpage({
-  sectionsColor: ['#B8AE9C', '#348899', '#F2AE72', '#5C832F', '#B8B89F'],
+  sectionsColor: ['#0d3c87', '#0d3c87', '#0d3c87', '#0d3c87', '#0d3c87'],
   sectionSelector: '.vertical-scrolling',
   slideSelector: '.horizontal-scrolling',
   navigation: true,

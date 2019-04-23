c22ff3f33a4dc1d75da48802e7a16a11a81d772d
Geri Rosenberg <gbrosenberg@wisc.edu>
2019-04-23 17:42:01 -0400

    added county boundaries

--- js/geojson.js
+++ js/geojson.js
@@ -1,14 +1,14 @@
 /* Map of GeoJSON data from counties.geojson */
 
-//function to instantiate the Leaflet map
+// function to instantiate the Leaflet map
 function createMap(){
-    //create the map
+    // create the map
     var map = L.map('section3', {
         center: [39, -98],
         zoom: 4
     });
 
-    //add OSM base tilelayer
+    // add OSM base tilelayer
     L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicHNteXRoMiIsImEiOiJjaXNmNGV0bGcwMG56MnludnhyN3Y5OHN4In0.xsZgj8hsNPzjb91F31-rYA', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18,
@@ -17,4 +17,8 @@ function createMap(){
     }).addTo(map);
 
 };
+
+// add county data
+L.geoJson(countyBoundaries).addTo(map);
+
 $(document).ready(createMap);

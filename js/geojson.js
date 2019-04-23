988e17553b8a0a7104e21bcc3e7a593789c8ecb4
Geri Rosenberg <gbrosenberg@wisc.edu>
2019-04-23 17:54:15 -0400

    added HPSA

--- js/geojson.js
+++ js/geojson.js
@@ -1,24 +1,24 @@
 /* Map of GeoJSON data from counties.geojson */
 
 // function to instantiate the Leaflet map
-// function createMap(){
-// create the map
-var map = L.map('section3', {
-    center: [37.8, -96],
-    zoom: 4
-});
+function createMap(){
+    // create the map
+    var map = L.map('section3', {
+        center: [37.8, -96],
+        zoom: 4
+    });
 
-// add OSM base tilelayer
-L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicHNteXRoMiIsImEiOiJjaXNmNGV0bGcwMG56MnludnhyN3Y5OHN4In0.xsZgj8hsNPzjb91F31-rYA', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoibWF0dHJvZGUiLCJhIjoiY2pzdWdsNnJvMDJuODQ5b2VydTBuYWF4dCJ9.4RfNabbj_uH0TcKSACZ_Lw'
-}).addTo(map);
+    // add OSM base tilelayer
+    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicHNteXRoMiIsImEiOiJjaXNmNGV0bGcwMG56MnludnhyN3Y5OHN4In0.xsZgj8hsNPzjb91F31-rYA', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoibWF0dHJvZGUiLCJhIjoiY2pzdWdsNnJvMDJuODQ5b2VydTBuYWF4dCJ9.4RfNabbj_uH0TcKSACZ_Lw'
+    }).addTo(map);
 
-// };
+};
 
 // add county data
-L.geoJson(countyBoundaries).addTo(map);
+L.geoJson(countyHPSA).addTo(map);
 
-// $(document).ready(createMap);
+$(document).ready(createMap);

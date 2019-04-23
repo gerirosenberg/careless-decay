21a8b36303e666b333337fedac1fcb0072661e29
Geri Rosenberg <gbrosenberg@wisc.edu>
2019-04-23 17:32:49 -0400

    updated geojson.js

--- js/geojson.js
+++ js/geojson.js
@@ -1,17 +1,20 @@
 /* Map of GeoJSON data from counties.geojson */
 
-// function to instantiate the Leaflet map
-function createMap () {
-    // create the map
-    var mymap = window.L.map('mapid').setView([37.8, -96], 4);
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+//function to instantiate the Leaflet map
+function createMap(){
+    //create the map
+    var map = L.map('section3', {
+        center: [39, -98],
+        zoom: 4
+    });
+
+    //add OSM base tilelayer
+    L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoicHNteXRoMiIsImEiOiJjaXNmNGV0bGcwMG56MnludnhyN3Y5OHN4In0.xsZgj8hsNPzjb91F31-rYA', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18,
         id: 'mapbox.streets',
-        accessToken: 'pk.eyJ1IjoiZ2VyaXJvc2VuYmVyZyIsImEiOiJjanVvazRlYjczNDcwNDRwM29icjhpbjJlIn0.8_jND_wBkaMl7jbfn79dOg'
-            }).addTo(mymap);
+        accessToken: 'pk.eyJ1IjoibWF0dHJvZGUiLCJhIjoiY2pzdWdsNnJvMDJuODQ5b2VydTBuYWF4dCJ9.4RfNabbj_uH0TcKSACZ_Lw'
+    }).addTo(map);
 
-        // call getData function
-        getData(mymap);
 };
-
 $(document).ready(createMap);
